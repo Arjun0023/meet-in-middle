ff6fd84691258730207bcf583c1cba078d2d0456
Arjun Pawar <arjunp0023@gmail.com>
2024-10-05 01:09:11 +0530

    m

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -6,14 +6,14 @@ import {useNavigate} from "react-router-dom";
 const Signup = () => {
     const [formData, setformData ] = useState({email: " ", phoneNumber: " ", password: " "})
     const navigate = useNavigate();
-
+console.log(formData,"------------=")
     const handleInputChange = (e) => {
         setformData({...formData, [e.target.name] : e.target.value})
     }
     const handleSignup = async(e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:5001/api/signup', {
+            const response = await fetch('http://localhost:5001/api/auth/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -23,7 +23,7 @@ const Signup = () => {
                 const data = await response.json();
                 if(response.status === 201) {
                     alert(data.message);
-                    navigate('/otp-verification', {state:{ email: formData.email}});
+                    navigate('/otpVerification', {state:{ email: formData.email}});
                     }
                     else{
                         alert(data.message);
